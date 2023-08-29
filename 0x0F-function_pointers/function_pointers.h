aaa5a5eb75dc4474d669684def87c4ecb8fbcef6
Ibrahim-Opeyemi <opeyemiibrahimajibola.com>
2023-08-29 15:53:19 -0700

    Update: a program that performs simple operations

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -8,6 +8,5 @@ void print_name(char *name, void (*f)(char *s));
 int _putchar(char c);
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-int (*get_op_func(char *s))(int, int);
 
 #endif
